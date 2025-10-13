51aa61e48bd6432c6f5736f5ac4ec64b385b868d
IOTC Secretariat <iotc-statistics@fao.org>
2025-10-13 19:15:33 +0400

    Update iotc_base_common_plots_value_line.R
    Change from name = element_blank() to name = ""

--- R/iotc_base_common_plots_value_line.R
+++ R/iotc_base_common_plots_value_line.R
@@ -116,7 +116,7 @@ value_line = function(data,
     scale_y_continuous(expand = c(0, 0),
                        breaks = breaks_for(yMin, yMax),
                        labels = function(x) { format(x, big.mark = ",", scientific = FALSE) },
-                       sec.axis = dup_axis(name = element_blank())) +
+                       sec.axis = dup_axis(name = "")) +
 
     coord_cartesian(ylim = limit_for(yMin, yMax)) +
 
@@ -230,7 +230,7 @@ value_line_rel = function(data,
     scale_y_continuous(expand = c(0, 0),
                        breaks = seq(0, 100, 10),
                        labels = function(x) { format(x, big.mark = ",", scientific = FALSE) },
-                       sec.axis = dup_axis(name = element_blank())) +
+                       sec.axis = dup_axis(name = "")) +
 
     coord_cartesian(ylim = limit_for(yMin, yMax)) +
 
