bdcf91f31143ee30e39aa8a1057d117a38a4063d
eblondel <emmanuel.blondel1@gmail.com>
2026-09-18 23:03:28 +0200

    typo

--- R/iotc_base_common_plots_value_pareto.R
+++ R/iotc_base_common_plots_value_pareto.R
@@ -122,7 +122,7 @@ value_pareto = function(data,
 
   #Map to codelists
   if(!is.null(categorize_by_codelist) & !is(categorize_by_codelist, "try-error")){
-    category_levels = c(categorize_by_codelist[.(CODE = main_categories), on = "CODE", NAME_EN], ALL_OTHERS)
+    category_levels = c(categorize_by_codelist[.(CODE = major_categories), on = "CODE", NAME_EN], ALL_OTHERS)
     data_by_category_and_fill_all = data_by_category_and_fill_all |>
       dplyr::left_join(categorize_by_codelist, dplyr::join_by(CATEGORY == CODE)) |>
       dplyr::mutate(
