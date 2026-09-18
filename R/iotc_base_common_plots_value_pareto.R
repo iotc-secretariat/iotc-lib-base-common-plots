85c3bb639ec3b4af062b5e7a6b69ee8f5609c94e
eblondel <emmanuel.blondel1@gmail.com>
2026-09-18 22:41:30 +0200

    debug

--- R/iotc_base_common_plots_value_pareto.R
+++ R/iotc_base_common_plots_value_pareto.R
@@ -117,9 +117,6 @@ value_pareto = function(data,
   #Adjusts the value cumulative percentage in order to be able to plot the trendline with the right scale
   data_by_category_all[, VALUE_CUMULATIVE_PERCENTAGE_ADJ :=  VALUE_CUMULATIVE_PERCENTAGE * cMaxx / 100]
 
-  #Sets the order of the rows of the data by category and fill in descending value by category order
-  setorderv(data_by_category_and_fill_all, "VALUE_BY_CATEGORY", 1)
-
   #Map to codelists
   if(!is.null(categorize_by_codelist) & !is(categorize_by_codelist, "try-error")){
     data_by_category_and_fill_all = data_by_category_and_fill_all |>
@@ -139,6 +136,9 @@ value_pareto = function(data,
     data_by_category_and_fill_all[, CATEGORY := factor(CATEGORY, levels = c(major_categories, ALL_OTHERS), ordered = TRUE)]
     data_by_category_all         [, CATEGORY := factor(CATEGORY, levels = c(major_categories, ALL_OTHERS), ordered = TRUE)]
   }
+
+  #Sets the order of the rows of the data by category and fill in descending value by category order
+  setorderv(data_by_category_and_fill_all, "VALUE_BY_CATEGORY", 1)
 
   if(is.na(num_legend_rows))
     num_legend_rows = calculate_legend_rows(length(colors))
