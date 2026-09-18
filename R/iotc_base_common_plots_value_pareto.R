ba36ad7d184195ff4c8483340c903552312a4d56
eblondel <emmanuel.blondel1@gmail.com>
2026-09-18 23:24:34 +0200

    fix pareto

--- R/iotc_base_common_plots_value_pareto.R
+++ R/iotc_base_common_plots_value_pareto.R
@@ -160,7 +160,7 @@ value_pareto = function(data,
         FILL_BY = dplyr::coalesce(FILL_NAME, ALL_OTHERS)
       ) |>
       dplyr::select(-FILL_NAME)
-    data_by_category_and_fill_all = data_by_category_and_fill_all |>
+    data_by_category_all = data_by_category_all |>
       dplyr::left_join(
         dplyr::select(fill_by_codelist, CODE, FILL_NAME = NAME_EN),
         dplyr::join_by(FILL_BY == CODE)
