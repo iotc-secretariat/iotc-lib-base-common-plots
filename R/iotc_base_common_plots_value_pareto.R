c1f9f5bdfc4eb09c45d71ba700dad06d2fd13767
eblondel <emmanuel.blondel1@gmail.com>
2026-09-18 22:38:32 +0200

    debug pareto

--- R/iotc_base_common_plots_value_pareto.R
+++ R/iotc_base_common_plots_value_pareto.R
@@ -120,22 +120,24 @@ value_pareto = function(data,
   #Sets the order of the rows of the data by category and fill in descending value by category order
   setorderv(data_by_category_and_fill_all, "VALUE_BY_CATEGORY", 1)
 
-  #Factorizes the category column in the data by category and fill / data by category according to the major categories ordering
-  data_by_category_and_fill_all[, CATEGORY := factor(CATEGORY, levels = c(major_categories, ALL_OTHERS), ordered = TRUE)]
-  data_by_category_all         [, CATEGORY := factor(CATEGORY, levels = c(major_categories, ALL_OTHERS), ordered = TRUE)]
-
   #Map to codelists
   if(!is.null(categorize_by_codelist) & !is(categorize_by_codelist, "try-error")){
     data_by_category_and_fill_all = data_by_category_and_fill_all |>
       dplyr::left_join(categorize_by_codelist, dplyr::join_by(CATEGORY == CODE)) |>
       dplyr::mutate(
+        CATEGORY = factor(CATEGORY, levels = c(major_categories, ALL_OTHERS), ordered = TRUE),
         CATEGORY = dplyr::coalesce(NAME_EN, ALL_OTHERS)
       )
     data_by_category_all = data_by_category_all |>
       dplyr::left_join(categorize_by_codelist, dplyr::join_by(CATEGORY == CODE)) |>
       dplyr::mutate(
+        CATEGORY = factor(CATEGORY, levels = c(major_categories, ALL_OTHERS), ordered = TRUE),
         CATEGORY = dplyr::coalesce(NAME_EN, ALL_OTHERS)
       )
+  }else{
+    #Factorizes the category column in the data by category and fill / data by category according to the major categories ordering
+    data_by_category_and_fill_all[, CATEGORY := factor(CATEGORY, levels = c(major_categories, ALL_OTHERS), ordered = TRUE)]
+    data_by_category_all         [, CATEGORY := factor(CATEGORY, levels = c(major_categories, ALL_OTHERS), ordered = TRUE)]
   }
 
   if(is.na(num_legend_rows))
